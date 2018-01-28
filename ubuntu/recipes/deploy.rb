85e442997ca61571c934723a051c66555b98b934
ideahitme <yerken.tussupbekov@gmail.com>
2018-01-29 00:35:10 +0100

    do not use application

--- ubuntu/recipes/deploy.rb
+++ ubuntu/recipes/deploy.rb
@@ -12,13 +12,12 @@ end
 app = search(:aws_opsworks_app).first
 app_path = "/srv/#{app['shortname']}"
 
-application 'my-app' do
-  git app_path do
-    repository app["app_source"]["url"]
-    revision app["app_source"]["revision"]
-  end
+git app_path do
+  repository app["app_source"]["url"]
+  revision app["app_source"]["revision"]
+  action :sync
+end
 
-  execute 'run cmake' do
-    command "cd #{app_path} && make build"
-  end
+execute 'run cmake' do
+  command "cd #{app_path} && make build"
 end
