a8e2445ec2dc5634eed4c954c4bee657295858b1
ideahitme <yerken.tussupbekov@gmail.com>
2018-01-29 02:11:14 +0100

    fix pat for db file

--- ubuntu/recipes/db.rb
+++ ubuntu/recipes/db.rb
@@ -1,5 +1,5 @@
 rds_db_instance = search("aws_opsworks_rds_db_instance").first
 
-file '/svc/conn.json' do
+file '/srv/conn.json' do
   content "{\"user\":\"#{rds_db_instance['db_user']}\",\"password\":\"#{rds_db_instance['db_password']}\", \"address\":\"#{rds_db_instance['address']}\", \"port\":\"#{rds_db_instance['port']}\"}"
 end
